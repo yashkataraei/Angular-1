267b8917fcc5f5668f5683e3f40adf3cd1aa49f2
yashkataraei <yashkataraei@gmail.com>
2025-02-19 16:32:06 +0530

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+// import { Ripple, initMDB } from "mdb-ui-kit";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // Ripple,
+    // initMDB
   ],
   providers: [],
   bootstrap: [AppComponent]
